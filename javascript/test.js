0d3fbadbca082a4e72acd8b345d063dc00928a23
marcus <markzcollege404@gmail.com>
2025-04-10 12:40:36 -0400

    ethers.js signer not being used

--- javascript/test.js
+++ javascript/test.js
@@ -3,12 +3,11 @@ const ethers = require("ethers") // npm i ethers@5.7.2 https://github.com/smartc
 const rpcURL = "https://rpc.dev.gblend.xyz/" // Your RPC URL goes here
 
 const provider = new ethers.providers.JsonRpcProvider(rpcURL)
-const signer = new ethers.Wallet(Buffer.from(process.env.devTestnetPrivateKey, 'hex'), provider);
 
 const contractAddress = '0xeC9a237864f7e78fd835Db717DB4e3d3c4254b11'
 const contractABI = [{"inputs":[{"internalType":"address","name":"FluentRustAddress","type":"address"}],"stateMutability":"nonpayable","type":"constructor"},{"inputs":[],"name":"fluentRust","outputs":[{"internalType":"contractIFluentRust","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getRustAddress","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getRustBool","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getRustBytes","outputs":[{"internalType":"bytes","name":"","type":"bytes"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getRustBytes32","outputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getRustInt256","outputs":[{"internalType":"int256","name":"","type":"int256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getRustString","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getRustUint256","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"}]
 
-const contractDeployed = new ethers.Contract(contractAddress, contractABI, signer);
+const contractDeployed = new ethers.Contract(contractAddress, contractABI);
 
 let fluent_sepolia_chain_id = 20993;
 
