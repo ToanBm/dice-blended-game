7ac92260f6c03fec6af8614b98fb176bd27385a3
marcus <markzcollege404@gmail.com>
2025-05-20 15:29:14 -0400

    Solidity version 0.8.30

--- solidity/src/FluentSdkRustTypesTest.sol
+++ solidity/src/FluentSdkRustTypesTest.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.28;
+pragma solidity 0.8.30;
 
 interface IFluentRust {
     // Make sure type interfaces are defined here or else there will be a compiler error.
